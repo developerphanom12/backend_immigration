c93e05fa27ed7b7e45e6aae64f9136965bda42ed
developer <developer.phanomprofessionals@gmail.com>
2023-11-16 17:05:15 +0530

    test

--- routes/universityRoutes.js
+++ routes/universityRoutes.js
@@ -1,44 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const univeristy = require('../service/univesityService')
-const {universityValid, coursesValid, coursenewschemma, ugschema} = require('../validation/validation');
+const { universityValid, coursesValid, coursenewschemma, ugschema } = require('../validation/validation');
 const authenticateToken = require("../service/token");
 const { upload } = require('../service/multer');
-  
+
 //******************************university routes********************************** *//
 
-router.get('/get1233333333333', authenticateToken,univeristy.getallacoursebyid) //---------->>> //by ambassdor new api
-router.get('/getallug', authenticateToken,univeristy.getallugrequirement) //---------->>> //by ambassdor new api
-router.get('/getallpg', authenticateToken,univeristy.getallpgrequirement) //---------->>> //by ambassdor new api
 
+router.get('/getallug', authenticateToken, univeristy.getallugrequirement) //--->>get al ug courses requirement
+
+router.get('/getallpg', authenticateToken, univeristy.getallpgrequirement) //---------->>> get al pg courses requirement
 
 router.post('/register', upload.fields([{ name: 'registration_certificate' }, { name: 'university_image' }]), univeristy.registerUniversityAndUploadImage);
 
-router.post('/unilogin',univeristy.uniersitylogin)
+router.post('/unilogin', univeristy.uniersitylogin)
 
-router.post('/universitys',authenticateToken, universityValid,univeristy.registerUniversity)
+router.post('/universitys', authenticateToken, universityValid, univeristy.registerUniversity)
 
-router.get('/:id' ,  univeristy.getUniversityByIdHandler)
+router.get('/:id', univeristy.getUniversityByIdHandler)
 
 router.get('/getall/university', authenticateToken, univeristy.getalluniversity) //------------>>>>>s
 
-  
+
 router.put('/updateUniversity/:id', univeristy.updateUniversity1);
- 
+
 
 router.put('/image/:id', upload.single('university_image'), univeristy.uploadImage1)
 
 
 /******************************Courses Api********************************************************** */
-    
-router.post('/newcoursesadd',authenticateToken, coursenewschemma,univeristy.courseadd) //---------->>> //by ambassdor new api
 
-router.post('/ugrequirement',authenticateToken, ugschema,univeristy.ugRequirement) //---------->>> //by ambassdor new api
+router.post('/newcoursesadd', authenticateToken, coursenewschemma, univeristy.courseadd) //---------->>> //by ambassdor new api
 
-router.post('/pgrequirement',authenticateToken,ugschema,univeristy.pgRequirement) //---------->>> //by ambassdor new api
+router.post('/ugrequirement', authenticateToken, ugschema, univeristy.ugRequirement) //---------->>> //by ambassdor new api
+
+router.post('/pgrequirement', authenticateToken, ugschema, univeristy.pgRequirement) //---------->>> //by ambassdor new api
 
 
-router.post('/courses1' ,coursesValid,authenticateToken, univeristy.courseCreate)
+router.post('/courses1', coursesValid, authenticateToken, univeristy.courseCreate)
 
 
 router.get('/get/allcourse', univeristy.getAllCoursesHandler);
@@ -50,6 +50,11 @@ router.get('/all/courseonly', authenticateToken, univeristy.getallcourses1)   //
 
 router.get('/get/:id', univeristy.getById)
 
+
+
+
+
+router.get('/getdatabyme', authenticateToken, univeristy.getallacoursebyid) //-->>>get all courses
 
 
 module.exports = router;
