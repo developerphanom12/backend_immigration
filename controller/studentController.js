8c7b9434993c89bc37af01bf9504892fc3d96247
developer <developer.phanomprofessionals@gmail.com>
2023-12-05 12:01:22 +0530

    stu

--- controller/studentController.js
+++ controller/studentController.js
@@ -132,7 +132,7 @@ const transporter = nodemailer.createTransport({
   service: 'Gmail', 
   auth: {
     user: 'ashimavineet2729@gmail.com',
-    pass: 'lvzi nwhx jzng oumz',   ////---->>>>>app password  from google
+    pass: 'suqo spfj ajsb fieb',   ////---->>>>>app password  from google
   },
 });
 
