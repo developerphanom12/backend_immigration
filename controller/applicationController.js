f378209e0b2a22a688cc7a1ec3aa99b4cc75d734
developer <developer.phanomprofessionals@gmail.com>
2023-12-11 10:49:51 +0530

    commentess

--- controller/applicationController.js
+++ controller/applicationController.js
@@ -449,7 +449,7 @@ async function getbyid(applicationId) {
   d.file_path,
   c.course_id AS course_id,
   c.course_name,
-  c.course_level,
+  c.course_type,
   c.update_date,
   cc.id AS comment_id,
   cc.comment_text,
@@ -467,7 +467,7 @@ LEFT JOIN user01 u ON a.user_id = u.id AND a.role = 'user'
 LEFT JOIN students s ON a.user_id = s.id AND a.role = 'student'
 LEFT JOIN university au ON a.university_id = au.university_id
 LEFT JOIN documnets d ON a.application_id = d.application_id
-LEFT JOIN courses c ON a.course_id = c.course_id 
+LEFT JOIN courses_list c ON a.course_id = c.course_id 
 LEFT JOIN comment_table cc ON cc.application_id = a.application_id
 LEFT JOIN staff staff ON cc.role = 'staff' AND staff.id = cc.user_id
 LEFT JOIN admintable ad ON cc.role = 'admin' AND ad.id = cc.user_id
